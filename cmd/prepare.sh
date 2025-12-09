df3a9f1235870cecca42913b9ed88901733c8e79
rachmathasibuan <rachmathasibuan@gmail.com>
2025-12-09 15:14:28 +0700

    update file

--- cmd/prepare.sh
+++ cmd/prepare.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 BACKUPDB="/home/ubuntu/backupdb"
-BACKUP_FILE="/var/opt/mssql/data"  
+BACKUP_FILE="/var/opt/mssql/data"
+BACKUP_NFS="/mnt/backup"
 FILE="E_FRM2_MGP"
 date=$(date +"%Y-%m-%d")
+preparelog="/home/ubuntu/backupdb/cmd/week.log"
+
+echo "***** date $(date '+%Y-%m-%d %H:%M:%S') *****" >> "$preparelog"
+
 
 find $BACKUP_FILE -type f -name "*.bak" -exec rm {} \;
 find $BACKUPDB -type f -name "$FILE-$date.bak.tar.gz" -exec tar -zxvf {} -C $BACKUP_FILE \;
 find $BACKUP_FILE -type f -name "*.bak" -exec chown mssql:mssql {} \;
-find $BACKUPDB -type f -mtime +7 -name "*.bak.tar.gz" -exec rm {} \;
+
+find $BACKUPDB -type f -name "$FILE-$date.bak.tar.gz" -exec cp {} $BACKUP_NFS \;
+find $BACKUPDB -type f -name "test-$date.sql.tar.gz" -exec cp {} $BACKUP_NFS \;
+
+find $BACKUPDB -type f -mtime +3 -name "*.bak.tar.gz" -exec rm {} \;
+find $BACKUPDB -type f -mtime +3 -name "test-$date.sql.tar.gz" -exec rm {} \;
+find $BACKUP_NFS -type f -mtime +5 -name "*.bak.tar.gz" -exec rm {} \;
+find $BACKUP_NFS -type f -mtime +5 -name "test-$date.sql.tar.gz" -exec rm {} \;
